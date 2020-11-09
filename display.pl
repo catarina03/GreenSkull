aed5e6c7dce12e77ce8f8f4cce33b668776b714a
jessymireie <up201806723@fe.up.pt>
2020-11-09 00:09:00 +0000

    changed board display, not done

--- display.pl
+++ display.pl
@@ -12,6 +12,7 @@
 % GreenSkull - who has the Green Skull
 display_game(GameState-GreenSkull, Player) :-
     nl,nl,print_board(GameState,0),
+    hex_code(10,C),write(C),nl,nl,
     display_column_numbers,
     display_green_skull(GreenSkull), 
     display_player_turn(Player),
@@ -33,13 +34,14 @@ display_green_skull(g) :-
 
 % Prints the full board
 print_board([],N).
-print_board([H|T],N) :-
+print_board([H|T],N) :- 
+    hex_code(N,C),write(C),nl,
     space(N,S),write(S),
     write('|'), print_row(H),
     N1 is N+1,
-    nl,nl,
+    nl,
     print_board(T,N1).
-    
+
 % Prints a single row
 print_row([]).
 print_row([H|T]):-
