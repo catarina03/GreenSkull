f9c15e0e55b0c2a42727a656eab2eada1d710b54
Catarina <up201806610@fe.up.pt>
2020-11-20 16:51:16 +0000

    Appearance improved

--- data.pl
+++ data.pl
@@ -9,16 +9,16 @@ player(g).
 player(z).
 
 % Replaces number with string with spaces and row number.
-space(0,' 1                           ').
-space(1,' 2                         ').
-space(2,' 3                       ').
-space(3,' 4                     ').
-space(4,' 5                   ').
-space(5,' 6                 ').
-space(6,' 7               ').
-space(7,' 8             ').
-space(8,' 9           ').
-space(9,'10         ').
+space(0,'                     1 |     ').
+space(1,'                   2 |     ').
+space(2,'                 3 |     ').
+space(3,'               4 |     ').
+space(4,'             5 |     ').
+space(5,'           6 |     ').
+space(6,'         7 |     ').
+space(7,'       8 |     ').
+space(8,'     9 |     ').
+space(9,'  10 |     ').
 
 
 hex_code(0,'                        /     / \\     \\').
--- display.pl
+++ display.pl
@@ -13,7 +13,7 @@
 display_game(GameState-GreenSkull, Player) :-
     nl,nl,print_board(GameState,0),
     hex_code(10,C),write(C),nl,nl,
-    write('                          z o m b i e s'),nl,nl,
+    write('                         z o m b i e s'),nl,
     display_column_numbers,
     display_green_skull(GreenSkull), 
     display_player_turn(Player),
@@ -21,24 +21,24 @@ display_game(GameState-GreenSkull, Player) :-
 
 % Display who plays this round
 display_player_turn(o):- 
-    nl, write('                 TURN TO PLAY, ORCS!').
+    nl, write('                     TURN TO PLAY, ORCS!').
 display_player_turn(g):-  
-    nl, write('                TURN TO PLAY, GOBLINS!').
+    nl, write('                    TURN TO PLAY, GOBLINS!').
 display_player_turn(z):-
-    nl, write('                TURN TO PLAY, ZOMBIES!').
+    nl, write('                    TURN TO PLAY, ZOMBIES!').
 
 % Display who has the Green Skull
 display_green_skull(o) :-
-    nl, write('              ORCS HAVE THE GREEN SKULL!').
+    nl, write('                  ORCS HAVE THE GREEN SKULL!').
 display_green_skull(g) :-
-    nl, write('             GOBLINS HAVE THE GREEN SKULL!').
+    nl, write('                 GOBLINS HAVE THE GREEN SKULL!').
 
 % Prints the full board
 print_board([],N).
 print_board([H|T],N) :- 
     hex_code(N,C),write(C),nl,
     space(N,S),write(S),
-    write('|'), print_row(H),
+    write('|'), print_row(H), write('     | '),
     N1 is N+1,
     nl,
     print_board(T,N1).
@@ -53,7 +53,9 @@ print_row([H|T]):-
 
 % Prints column numbers
 display_column_numbers:-
-    write('            1   2   3   4   5   6   7   8   9   10'),
+%   write('            \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ /   '), nl, nl,
+ write('        - - - - - - - - - - - - - - - - - - - - - - - -   '), nl, nl,
+    write('       1   2   3   4   5   6   7   8   9   10'),
     nl.
     
 % Displays game over message
